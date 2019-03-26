a4e36d15cb9940a77b78fb5b398b8ec4b08d7c59
Tha.alhathanni <tha.alhathanni@gmail.com>
2019-03-26 16:31:28 +0300

    homework

--- starter-code/json/app.js
+++ starter-code/json/app.js
@@ -23,11 +23,17 @@ var notActuallyJSON = {
 };
 // [ Step 1 ] Use JSONLint to determine if something is valid JSON.
 // Copy the entire object above ({...}, but NOT 'var notActuallyJSON =' or the trailing semicolon)--just copy everything between the brackets, including the brackets themselves.
-// Then go to http://jsonlint.com/ and paste it into the validator. Does it pass the test of being valid JSON?
+// Then go to http://jsonlint.com/ and paste it into the validator. Does it pass the test of being valid JSON? Yes it does pass the test of being valid JSON.
 
 // The following line is valid JavaScript, but the object is not written in valid JSON syntax.
 // [ Step 2 ] Use JSONLint to validate the object in the following line; then, edit the object so it passes the validator.
 var eddie = {name:"Eddie Vedder", age:49};
+
+
+ {
+    "name": "Eddie Vedder",
+    "age": 49
+ }
 
 // We frequently want to turn a JavaScript object into a JSON string, and vice versa.
 // JavaScript has a built-in global object called `JSON` that contains a number of useful methods for manipulating JSON.
@@ -35,9 +41,45 @@ var eddie = {name:"Eddie Vedder", age:49};
 //      (See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify)
 
 // [ Step 3 ] Turn the `grungeAlbums` JavaScript object into a JSON string, and store it in `grungeAlbumsJSON`.
-// Then, convert `grungeAlbumsJSON` back into a JavaScript object, and compare it to the original `grungeAlbums`. Are they the same?
+// Then, convert `grungeAlbumsJSON` back into a JavaScript object, and compare it to the original `grungeAlbums`. Are they the same? No there are not. how ever it kind close to each other same data but diferent format.
 
 var grungeAlbums = {
+  "albums":[
+    {
+        "name": "Bleach",
+        "artist": "Nirvana",
+        "unitsSold": 1700000
+    },
+    {
+        "name": "Nevermind",
+        "artist": "Nirvana",
+        "unitsSold": 30000000
+    },
+    {
+        "name": "In Utero",
+        "artist": "Nirvana",
+        "unitsSold": 15000000
+    },
+    {
+        "name": "Ten",
+        "artist": "Pearl Jam",
+        "unitsSold": 10000000
+    },
+    {
+        "name": "Vs",
+        "artist": "Pearl Jam",
+        "unitsSold": 6100000
+    },
+    {
+        "name": "Vitalogy",
+        "artist": "Pearl Jam",
+        "unitsSold": 4770000
+    }
+  ]
+};
+
+JSON.stringify(grungeAlbumsJSON);
+{
   "albums":[
     {
         "name": "Bleach",
